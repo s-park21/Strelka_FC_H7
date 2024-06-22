9462dad84789776abd19165c4922ecb8567d78c6
s-park21 <theangusmclennan@gmail.com>
2024-06-22 11:51:34 +1000

    Put HITL code into library file

--- Core/Inc/gitcommit.h
+++ Core/Inc/gitcommit.h
@@ -1,2 +1,2 @@
 #define GIT_INFO_PRESENT
- static const char* GIT_INFO = "Version Information=[779ef63360f0e06db18fedd4e3d571ba5cec8c56, (HEAD -> HITL, origin/HITL)]\r\n";
+ static const char* GIT_INFO = "Version Information=[1342c21a8bc4097f2f447465faab38d890fecf26, (HEAD -> HITL, origin/HITL)]\r\n";
